aa8b4679e126c9438770b3b8d4790c2b86e7ecef
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-01-17 16:52:48 -0300

    FEAT: start blog

--- app/(site)/aboutUs/page.tsx
+++ app/(site)/aboutUs/page.tsx
@@ -20,25 +20,26 @@ const AboutPage = () => {
         <div className="w-full flex justify-around px-5 gap-5 my-5">
             <div className="w-1/4">
                 <div>
-                    <HiOutlineFlag size={25} className="text-primary"/>
+                    <HiOutlineFlag size={28} className="text-primary"/>
                     <h1 className="font-semibold">Missão</h1>
                 </div>
-                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quaerat obcaecati nihil, consequatur doloremque deleniti nesciunt libero hic nam tempora quos quam quisquam impedit suscipit iste, corrupti porro? Laboriosam, cum?</p>
+                <p className="text-justify">Criar e implementar soluções digitais, que otimize custos, organize processos, potencialize talentos, fortaleça a cultura organizacional e impulsione a transformação da Gestão Estratégica de Pessoas</p>
             </div>
             <div className="w-1/4">
                 <div>
-                  <HiOutlineEye size={25} className="text-primary"/>
-                  <h1 className="font-semibold">Visão</h1>
-                </div>
-                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quaerat obcaecati nihil, consequatur doloremque deleniti nesciunt libero hic nam tempora quos quam quisquam impedit suscipit iste, corrupti porro? Laboriosam, cum?</p>
-            </div>
-            <div className="w-1/4">
-                <div>
-                  <HiOutlineScale size={25} className="text-primary"/>
+                  <HiOutlineScale size={28} className="text-primary"/>
                   <h1 className="font-semibold">Valores</h1>
                 </div>
-                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quaerat obcaecati nihil, consequatur doloremque deleniti nesciunt libero hic nam tempora quos quam quisquam impedit suscipit iste, corrupti porro? Laboriosam, cum?</p>
+                <p className="text-justify">Acreditamos na harmonia entre tecnologia, pessoas e processos, criando um ambiente onde todos os elementos se integram e impulsionam um desevolvimento sustentável. Inovação Valorizamos a busca constante por novas soluções, promovendo práticas que transformam positivamente a Gestão Estratégica de Pessoas. Sustentabilidade Acreditamos que práticas sustentáveis ​​são essenciais para garantir um futuro próspero e responsável para as empresas e a sociedade</p>
             </div>
+            <div className="w-1/4">
+                <div>
+                  <HiOutlineEye size={28} className="text-primary"/>
+                  <h1 className="font-semibold">Visão</h1>
+                </div>
+                <p className="text-justify">Ser referência na criação de soluções digitais para a Gestão Estratégica de Pessoas, inspirando ambientes,  inovadores, conectados e sustentáveis, apoiados por uma visão holística integrada</p>
+            </div>
+          
         </div>
         {/* <div className="w-full ml-16">
             <Integration />
