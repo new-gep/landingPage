13485e8e35e51190b37021e60a93d4ea4149cccf
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-02-20 17:56:02 -0300

    REFACTOR: Optimize lead API endpoint URL and remove debug logging

--- api/post/lead/index.ts
+++ api/post/lead/index.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 export const Default = async (data: any) => {
     try{
-        console.log(`${process.env.NEXT_PUBLIC_API_URL}/lead`)
-        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/lead`, data);
+        // console.log(`${process.env.NEXT_PUBLIC_API_URL}lead`)
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}lead`, data);
+        // const response = await axios.post(`http://localhost:9008/lead`, data);
         return response.data;
     }catch(error){
         console.log(error);
